ae82c89aafcf140b4e99eed8cebe54bd0645b902
정보경 <51705875+gonpress@users.noreply.github.com>
2023-03-20 23:41:05 +0900

    todo 내용 추가

--- lib/main.dart
+++ lib/main.dart
@@ -47,16 +47,19 @@ class _TodoListState extends State<TodoList> {
                 child: Row(
                   children: [
                     Expanded(
+                      // TODO: 왼쪽 화살표를 누르면 이전 날짜로 넘어가도록 한다.
                       child: IconButton(
                           icon: Icon(Icons.arrow_circle_left_sharp, size: 40.0),
                           onPressed: () {}),
                     ),
                     Expanded(
+                      // TODO: 날짜를 누르면 달력이 뜨도록 한다.
                       child: Text(DateTime.now().toString().substring(0, 10),
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.w400)),
                     ),
                     Expanded(
+                      // TODO: 오른쪽 화살표를 누르면 다음 날짜로 넘어가도록 한다.
                       child: IconButton(
                           icon:
                               Icon(Icons.arrow_circle_right_sharp, size: 40.0),
@@ -88,6 +91,10 @@ class _TodoListState extends State<TodoList> {
                     Expanded(
                         flex: 1,
                         child: ElevatedButton(
+                          // TODO: 추가를 누르면 렌더링이 새로되어야 한다.
+                          // 자료의 구조는 할일 내용과 할일 완료여부를 가져야 한다.
+                          // 할일 완료여부는 기본값으로 false를 가져야 한다.
+                          // 데이터는 로컬스토리지로 기기에 저장하자.
                           onPressed: () {
                             todoList.add(todoItem.text);
                             print('todoList: $todoList');
@@ -108,6 +115,8 @@ class _TodoListState extends State<TodoList> {
               ),
               Expanded(
                 child: ListView(
+                  // TODO: 배열에 값이 추가되면 렌더링이 되어야 한다.
+                  // TODO: 리스트 타일은 체크박스를 가지며 완료여부에 대한 상태를 가지고 있어야 한다.
                   children: todoList.map((todo) {
                     return ListTile(
                       title: Text(todo),
@@ -129,9 +138,13 @@ class _TodoListState extends State<TodoList> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                // TODO: 전체, 진행 중, 완료, 전체 삭제 버튼을 만들고 동작하게 한다.
                 TextButton(onPressed: () {}, child: Text('전체')),
                 TextButton(onPressed: () {}, child: Text('진행 중')),
                 TextButton(onPressed: () {}, child: Text('완료')),
+
+                // TODO: 전체 삭제 버튼을 누르면 배열을 비우고 렌더링이 되어야 한다.
+                // 실수로 누르는 경우를 대비해서 버튼의 스타일을 변경하고 우측으로 정렬하자.
                 TextButton(onPressed: () {}, child: Text('전체 삭제')),
               ],
             ),
