956520979fe6fafcbd776619471d62396aedc8e8
정보경 <51705875+gonpress@users.noreply.github.com>
2023-03-20 23:36:44 +0900

    todo list 레이아웃 작성

--- lib/main.dart
+++ lib/main.dart
@@ -15,56 +15,127 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoList> createState() => _TodoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _TodoListState extends State<TodoList> {
+  TextEditingController todoItem = TextEditingController();
+  var todoList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo List'),
-        backgroundColor: Colors.blueGrey[800],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              DateTime.now().toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Todo List'),
+          backgroundColor: Colors.blueGrey[700],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: IconButton(
+                          icon: Icon(Icons.arrow_circle_left_sharp, size: 40.0),
+                          onPressed: () {}),
+                    ),
+                    Expanded(
+                      child: Text(DateTime.now().toString().substring(0, 10),
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w400)),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                          icon:
+                              Icon(Icons.arrow_circle_right_sharp, size: 40.0),
+                          onPressed: () {}),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: SizedBox(
+                        height: 50,
+                        child: TextField(
+                          controller: todoItem,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: '할 일을 입력하세요',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                        flex: 1,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            todoList.add(todoItem.text);
+                            print('todoList: $todoList');
+                          },
+                          child: Text('추가', style: TextStyle(fontSize: 16)),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blueGrey[700],
+                            minimumSize: const Size(60, 50),
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 10,
+                thickness: 1,
+                color: Colors.grey,
+              ),
+              Expanded(
+                child: ListView(
+                  children: todoList.map((todo) {
+                    return ListTile(
+                      title: Text(todo),
+                      trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            todoList.removeAt(todoList.indexOf(todo));
+                            print('todoList: $todoList');
+                          }),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: SizedBox(
+          child: BottomAppBar(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(onPressed: () {}, child: Text('전체')),
+                TextButton(onPressed: () {}, child: Text('진행 중')),
+                TextButton(onPressed: () {}, child: Text('완료')),
+                TextButton(onPressed: () {}, child: Text('전체 삭제')),
+              ],
+            ),
+          ),
+        ));
   }
 }
