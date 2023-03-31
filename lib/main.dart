80c9b37a345e2cff403e8797789e071497ee9fae
정보경 <bokyung.jung@medistream.co.kr>
2023-03-31 11:09:35 +0900

    setState 적용

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,14 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-  TextEditingController todoItem = TextEditingController();
+  late TextEditingController todoController;
   var todoList = [];
+
+  @override
+  void initState() {
+    todoController = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,100 +44,107 @@ class _TodoListState extends State<TodoList> {
           title: const Text('Todo List'),
           backgroundColor: Colors.blueGrey[700],
         ),
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      // TODO: 왼쪽 화살표를 누르면 이전 날짜로 넘어가도록 한다.
-                      child: IconButton(
-                          icon: Icon(Icons.arrow_circle_left_sharp, size: 40.0),
-                          onPressed: () {}),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                Expanded(
+                  // TODO: 왼쪽 화살표를 누르면 이전 날짜로 넘어가도록 한다.
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_circle_left_sharp, size: 40.0),
+                      onPressed: () {},
                     ),
-                    Expanded(
-                      // TODO: 날짜를 누르면 달력이 뜨도록 한다.
-                      child: Text(DateTime.now().toString().substring(0, 10),
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400)),
-                    ),
-                    Expanded(
-                      // TODO: 오른쪽 화살표를 누르면 다음 날짜로 넘어가도록 한다.
-                      child: IconButton(
-                          icon:
-                              Icon(Icons.arrow_circle_right_sharp, size: 40.0),
-                          onPressed: () {}),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: SizedBox(
-                        height: 50,
-                        child: TextField(
-                          controller: todoItem,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: '할 일을 입력하세요',
-                          ),
-                        ),
+                Expanded(
+                  // TODO: 날짜를 누르면 달력이 뜨도록 한다.
+                  child: Text(
+                    DateTime.now().toString().substring(0, 10),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ),
+                Expanded(
+                  // TODO: 오른쪽 화살표를 누르면 다음 날짜로 넘어가도록 한다.
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_circle_right_sharp, size: 40.0),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: TextField(
+                      enableSuggestions: false,
+                      controller: todoController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '할 일을 입력하세요',
                       ),
                     ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Expanded(
-                        flex: 1,
-                        child: ElevatedButton(
-                          // TODO: 추가를 누르면 렌더링이 새로되어야 한다.
-                          // 자료의 구조는 할일 내용과 할일 완료여부를 가져야 한다.
-                          // 할일 완료여부는 기본값으로 false를 가져야 한다.
-                          // 데이터는 로컬스토리지로 기기에 저장하자.
-                          onPressed: () {
-                            todoList.add(todoItem.text);
-                            print('todoList: $todoList');
-                          },
-                          child: Text('추가', style: TextStyle(fontSize: 16)),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.blueGrey[700],
-                            minimumSize: const Size(60, 50),
-                          ),
-                        )),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                        // TODO: 추가를 누르면 렌더링이 새로되어야 한다.
+                        // 자료의 구조는 할일 내용과 할일 완료여부를 가져야 한다.
+                        // 할일 완료여부는 기본값으로 false를 가져야 한다.
+                        // 데이터는 로컬스토리지로 기기에 저장하자.
+                        onPressed: () {
+                          if (todoController.text.isEmpty) {
+                            return;
+                          }
+
+                          print('다음 값을 추가합니당 : ${todoController.text}');
+                          setState(() {
+                            todoList.add(todoController.text);
+                            todoController.clear();
+                          });
+                        },
+                        child: Text('추가', style: TextStyle(fontSize: 16)),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blueGrey[700],
+                          minimumSize: const Size(60, 50),
+                        ),
+                      )),
+                ],
               ),
-              Divider(
-                height: 10,
-                thickness: 1,
-                color: Colors.grey,
-              ),
-              Expanded(
-                child: ListView(
-                  // TODO: 배열에 값이 추가되면 렌더링이 되어야 한다.
-                  // TODO: 리스트 타일은 체크박스를 가지며 완료여부에 대한 상태를 가지고 있어야 한다.
-                  children: todoList.map((todo) {
-                    return ListTile(
-                      title: Text(todo),
-                      trailing: IconButton(
-                          icon: Icon(Icons.delete, color: Colors.red),
-                          onPressed: () {
+            ),
+            Divider(
+              height: 10,
+              thickness: 1,
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: ListView(
+                // TODO: 배열에 값이 추가되면 렌더링이 되어야 한다.
+                // TODO: 리스트 타일은 체크박스를 가지며 완료여부에 대한 상태를 가지고 있어야 한다.
+                children: todoList.map((todo) {
+                  return ListTile(
+                    title: Text(todo),
+                    trailing: IconButton(
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          setState(() {
                             todoList.removeAt(todoList.indexOf(todo));
-                            print('todoList: $todoList');
-                          }),
-                    );
-                  }).toList(),
-                ),
+                          });
+                          print('todoList: $todoList');
+                        }),
+                  );
+                }).toList(),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
         bottomNavigationBar: SizedBox(
           child: BottomAppBar(
@@ -145,7 +158,13 @@ class _TodoListState extends State<TodoList> {
 
                 // TODO: 전체 삭제 버튼을 누르면 배열을 비우고 렌더링이 되어야 한다.
                 // 실수로 누르는 경우를 대비해서 버튼의 스타일을 변경하고 우측으로 정렬하자.
-                TextButton(onPressed: () {}, child: Text('전체 삭제')),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        todoList.clear();
+                      });
+                    },
+                    child: Text('전체 삭제')),
               ],
             ),
           ),
